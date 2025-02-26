65ebfac6e76edac1bc2d20dfcebbbfa57f4ccaaa
pallibalaram <158497199+pallibalaram@users.noreply.github.com>
2025-02-26 17:56:00 +0530

    modified

--- catalogue.sh
+++ catalogue.sh
@@ -61,7 +61,7 @@ cd /app
 npm install &>> $LOG
 VALIDATE $? " npm installation"
 
-cp /c/Users/BALARAM/repo/catalogue.service /etc/systemd/system/catalogue.service &>> $LOG
+cp c/Users/BALARAM/repo/catalogue.service /etc/systemd/system/catalogue.service &>> $LOG
 VALIDATE $? "coping to catalogue service"
 
 systemctl daemon-reload &>> $LOG
