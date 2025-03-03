a6e8edac57291015ad0029bced1326c1cdf015ea
pallibalaram <158497199+pallibalaram@users.noreply.github.com>
2025-03-03 16:04:18 +0530

    modified

---
+++ roboshop.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+AMI=ami-0b4f379183e5706b9
+SG_ID=sg-040913050c9d0b18a
+ZONE_ID=Z05391511HI4N0V6H16ZN
+DOMIAN_NAME="pavandev.online"
+INSTANCES=( "mongodb" "catalogue" "cart" "user" "redis" "web" "mysql" "shipping" "rabbitmq" "payment" )
+for i in "${INSTANCES[@]}"
+do
+    if [ $i=mongodb ] || [ $i=shipping ] ||[ $i=mysql ]
+    then
+        INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+done
+IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE--security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+echo "$i: $IP_ADDRESS"
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+  {
+    "Comment": "Testing creating a record set"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "' $i'.'$DOMIAN_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'" $IP_ADDRESS "'"
+        }]
+      }
+    }]
+  }
+  '
+        
+
+done
