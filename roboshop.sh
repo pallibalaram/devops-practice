3da7e3d3453a0eb14f39e09f6bb9c26ed328b257
pallibalaram <158497199+pallibalaram@users.noreply.github.com>
2025-03-04 14:28:12 +0530

    modified

--- roboshop.sh
+++ roboshop.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
+
 AMI=ami-0b4f379183e5706b9
-SG_ID=sg-07f92a2d97f3700e2
-ZONE_ID=Z05391511HI4N0V6H16ZN
-DOMIAN_NAME="pavandev.online"
-INSTANCES=( "mongodb" "catalogue" "cart" "user" "redis" "web" "mysql" "shipping" "rabbitmq" "payment" )
+SG_ID=sg-07f92a2d97f3700e2 
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+ZONE_ID=Z05391511HI4N0V6H16ZN 
+DOMAIN_NAME="pavandev.online"
+
 for i in "${INSTANCES[@]}"
 do
-    if [ $i==mongodb ] || [ $i==shipping ] || [ $i==mysql ]
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t3.small"
     else
         INSTANCE_TYPE="t2.micro"
     fi
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE--security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
+
+    #create R53 record, make sure you delete existing record
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
     --change-batch '
-      {
-        "Comment": "Testing creating a record set"
+    {
+        "Comment": "Creating a record set for cognito endpoint"
         ,"Changes": [{
-          "Action"              : "UPSERT"
-          ,"ResourceRecordSet"  : {
-            "Name"              : "' $i'.'$DOMIAN_NAME'"
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
-                "Value"         : "'" $IP_ADDRESS "'"
+                "Value"         : "'$IP_ADDRESS'"
             }]
-          }
+        }
         }]
-      }
-      '
-        
-
+    }
+        '
 done
