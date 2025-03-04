6944d41630b0850ea6f0c39e150d284848805c85
pallibalaram <158497199+pallibalaram@users.noreply.github.com>
2025-03-04 15:15:10 +0530

    modified

--- roboshop.sh
+++ roboshop.sh
@@ -12,25 +12,25 @@ do
     else
         INSTANCE_TYPE="t2.micro"
     fi
-done
-IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE--security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
-echo "$i: $IP_ADDRESS"
-    aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
-    --change-batch '
-  {
-    "Comment": "Testing creating a record set"
-    ,"Changes": [{
-      "Action"              : "UPSERT"
-      ,"ResourceRecordSet"  : {
-        "Name"              : "' $i'.'$DOMIAN_NAME'"
-        ,"Type"             : "A"
-        ,"TTL"              : 1
-        ,"ResourceRecords"  : [{
-            "Value"         : "'" $IP_ADDRESS "'"
+
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE--security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    echo "$i: $IP_ADDRESS"
+        aws route53 change-resource-record-sets \
+        --hosted-zone-id $ZONE_ID \
+        --change-batch '
+      {
+        "Comment": "Testing creating a record set"
+        ,"Changes": [{
+          "Action"              : "UPSERT"
+          ,"ResourceRecordSet"  : {
+            "Name"              : "' $i'.'$DOMIAN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'" $IP_ADDRESS "'"
+            }]
+          }
         }]
       }
-    }]
-  }
-  '
+      '
 done
