38169dcd5d30c39c62515eadb638dcc24eacf223
pallibalaram <158497199+pallibalaram@users.noreply.github.com>
2025-03-04 14:21:00 +0530

    modified

--- roboshop.sh
+++ roboshop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 AMI=ami-0b4f379183e5706b9
-SG_ID=sg-07d8372baed539857
+SG_ID=sg-07f92a2d97f3700e2
 ZONE_ID=Z05391511HI4N0V6H16ZN
 DOMIAN_NAME="pavandev.online"
 INSTANCES=( "mongodb" "catalogue" "cart" "user" "redis" "web" "mysql" "shipping" "rabbitmq" "payment" )
