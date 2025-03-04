4b5156c8043bea1bc5ad31391bfb6ad8a872f8c3
pallibalaram <158497199+pallibalaram@users.noreply.github.com>
2025-03-04 11:55:35 +0530

    modified

--- roboshop.sh
+++ roboshop.sh
@@ -14,9 +14,9 @@ do
     fi
     IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE--security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
-        aws route53 change-resource-record-sets \
-        --hosted-zone-id $ZONE_ID \
-        --change-batch '
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
       {
         "Comment": "Testing creating a record set"
         ,"Changes": [{
