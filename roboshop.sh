992e6be598fc10737b24491aee7476a9fcec8275
pallibalaram <158497199+pallibalaram@users.noreply.github.com>
2025-03-04 15:13:27 +0530

    modified

--- roboshop.sh
+++ roboshop.sh
@@ -1,20 +1,36 @@
-#!/bin/bash
-
+##!/bin/bash
 AMI=ami-0b4f379183e5706b9
-SG_ID=sg-07f92a2d97f3700e2 
-INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-ZONE_ID=Z05391511HI4N0V6H16ZN 
-DOMAIN_NAME="pavandev.online"
-
+SG_ID=sg-07f92a2d97f3700e2
+ZONE_ID=Z05391511HI4N0V6H16ZN
+DOMIAN_NAME="pavandev.online"
+INSTANCES=( "mongodb" "catalogue" "cart" "user" "redis" "web" "mysql" "shipping" "rabbitmq" "payment" )
 for i in "${INSTANCES[@]}"
 do
-    echo "instance is : $i"
-    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    if [ $i=mongodb ] || [ $i=shipping ] ||[ $i=mysql ]
     then
         INSTANCE_TYPE="t3.small"
     else
         INSTANCE_TYPE="t2.micro"
     fi
-
-    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-07f92a2d97f3700e2 
+done
+IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE--security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+echo "$i: $IP_ADDRESS"
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+  {
+    "Comment": "Testing creating a record set"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "' $i'.'$DOMIAN_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'" $IP_ADDRESS "'"
+        }]
+      }
+    }]
+  }
+  '
 done
