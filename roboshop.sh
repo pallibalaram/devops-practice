29b7b9009dc34ab1bfb65a078d58538862ce2862
pallibalaram <158497199+pallibalaram@users.noreply.github.com>
2025-03-04 14:32:52 +0530

    modified

--- roboshop.sh
+++ roboshop.sh
@@ -27,7 +27,7 @@ do
         ,"Changes": [{
         "Action"              : "UPSERT"
         ,"ResourceRecordSet"  : {
-            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Name"              : "'$i'.'$DOMAIN_NAME'"
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
